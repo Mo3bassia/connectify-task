d9c89658565d93a45c7cecef4f4f12cb563fa543
Mohamed Abassia <mo3bassia@gmail.com>
2025-03-09 19:11:40 +0200

    add tailwind css library

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <button className="bg-green-300 hover:bg-green-500/80 transition text-xl px-5 py-3 rounded-lg cursor-pointer">
+        Check Tailwind!
+      </button>
+    </div>
+  );
+}
