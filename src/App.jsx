0ffc404ebc10496f60a9e8b4e02df8eacff2ab6e
Mohamed Abassia <mo3bassia@gmail.com>
2025-03-09 20:26:45 +0200

    checking dark/light mode toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
+  // زر لتبديل الثيم
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <Button onClick={toggleTheme}>Change theme!</Button>
     </div>
   );
 }
