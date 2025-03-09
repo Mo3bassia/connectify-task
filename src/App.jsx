9af3cf2ce28cd895900cd8d883e2e754e78b307c
Mohamed Abassia <mo3bassia@gmail.com>
2025-03-09 20:57:58 +0200

    install react router dom library and set up app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/login.jsx";
+import User from "./pages/User.jsx";
+import UserDetails from "./pages/UserDetails.jsx";
+import Post from "./pages/Post.jsx";
+import PostDetails from "./pages/PostDetails.jsx";
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
@@ -24,10 +31,22 @@ function App() {
       fetch("https://dummyjson.com/users?limit=0").then((res) => res.json()),
   });
 
-  console.log(data)
+  console.log(data);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<div>Home</div>} />
+          <Route path="login" element={<Login />} />
+          <Route path="user" element={<User />}>
+            <Route path=":id" element={<UserDetails />} />
+          </Route>
+          <Route path="post" element={<Post />}>
+            <Route path=":id" element={<PostDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <Button onClick={toggleTheme}>Change theme!</Button>
     </div>
   );
